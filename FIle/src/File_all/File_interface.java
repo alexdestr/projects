33e8857cb49063488a80c47c992588d45eaf3d41
VincentVega <alexdestr@gmail.com>
2019-10-03 23:35:21 +0300

    s

--- FIle/src/File_all/File_interface.java
+++
@@ -1,9 +0,0 @@
-package File_all;
-
-public interface File_interface {
-
-    public void create(String name);
-    public void delete(String name);
-    public void reName(String name, String newName);
-
-}
--- untitled5/src/Student/RegistrException.java
+++
@@ -1,9 +0,0 @@
-package Student;
-
-public class RegistrException extends  Exception {
-
-        public RegistrException(String i) {
-            super(i);
-        }
-
-}
